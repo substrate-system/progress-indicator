4b49dc9526969d2b326226f18718a0c36a99360b
nichoth <nichoth@nichoth.com>
2025-12-10 11:11:13 -0800

    update all dependencies, improve tests

--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,16 @@ import { ProgressIndicator } from '../src/index.js'
 
 ProgressIndicator.define()
 
+test('setup', () => {
+    document.body.innerHTML += `
+        <progress-indicator progress="0" stroke="8">
+            <div role="alert" aria-live="polite">
+                <p>Loading, please wait…</p>
+            </div>
+        </progress-indicator>
+    `
+})
+
 let el:ProgressIndicator
 test('Find the element', async t => {
     el = document.querySelector('progress-indicator') as ProgressIndicator
@@ -13,5 +23,11 @@ test('set the attribute', t => {
     el.setProgress(50)
     const progressCount = el.querySelector('[data-progress-count]') as
         HTMLElement
-    t.ok(progressCount.innerText.includes('50'), 'should set the attribute to 50')
+    t.ok(progressCount.innerText.includes('50'),
+        'should set the attribute to 50')
+})
+
+test('all done', () => {
+    // @ts-expect-error
+    window.testsFinished = true
 })
